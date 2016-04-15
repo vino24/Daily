0bc556a83c5a1f70ad5317c212ae110c091991fa
vino <liugq19931023@gmail.com>
2016-04-15 19:21:46 +0800

    webpack & react

---
+++ webpack/react-sample/js/hello.js
@@ -0,0 +1,9 @@
+var React = require('react');
+class hello extends React.Component {
+    render() {
+        return (
+            <h1>hello {this.props.name}!</h1>
+        );
+    }
+}
+module.exports = hello;

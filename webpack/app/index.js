3ac1dc98c7a298f70aa11396094e702da9ff9c85
vino <liugq19931023@gmail.com>
2016-04-18 14:54:48 +0800

    重置

--- webpack/app/index.js
+++
@@ -1,9 +0,0 @@
-require('./main.css');
-var sub = require('./sub');
-var $ = require('jquery');
-var moment =require('moment');
-var app  = document.createElement('div');
-app.innerHTML = '<h1>Hello World</h1>';
-document.body.appendChild(app);
-app.appendChild(sub());
-$('body').append('<p>look at me! now is '+ moment().format()+'</p>');
--- webpack/react-sample/js/hello.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-class hello extends React.Component {
-    render() {
-        return (
-            <h1>hello {this.props.name}!</h1>
-        );
-    }
-}
-module.exports = hello;
